c31a8aae528e4e58769f850620fa76938fc62cff
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2022-01-15 12:57:31 -0500

    accordian

--- components/molecules/container/StepSection/StepRight.tsx
+++ components/molecules/container/StepSection/StepRight.tsx
@@ -6,7 +6,7 @@ import { BsEnvelopeOpen } from 'react-icons/bs'
 
 const StepRight : React.FC = () => {
     const [isOpen, setIsOpen] = useState<any>({
-        accordion1: isOpen.accordion1,
+        accordion1: true,
         accordion2: false,
         accordion3: false
     })
