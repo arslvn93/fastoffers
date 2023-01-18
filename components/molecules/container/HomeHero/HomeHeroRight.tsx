27c207b7f415ae596489021958f0fab33c2891f1
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2023-01-18 18:53:30 -0500

    make homepage image bigger

--- components/molecules/container/HomeHero/HomeHeroRight.tsx
+++ components/molecules/container/HomeHero/HomeHeroRight.tsx
@@ -5,7 +5,7 @@ const HomeHeroRight : React.FC = () => {
         <div className='lg:pl-10 flex-grow-0 flex-shrink w-full lg:w-6/12 xl:w-6/12 2xl:w-7/12'>
             <div>
                 <img
-                    className='relative z-10 max-w-full xl:max-w-3xl mx-auto w-full anim-bounce-image'
+                    className='relative z-10 max-w-full xl:max-w-5xl mx-auto w-full anim-bounce-image'
                     src="./header-cartoon.png"
                     alt="cartoon header"
                 />
